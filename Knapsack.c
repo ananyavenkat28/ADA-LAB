44272942517820af67e3e8bd293c315468dbd600
ananyavenkat28 <ananya.cs24@bmsce.ac.in>
2026-05-08 11:35:08 +0530

    Add files via upload

---
+++ Knapsack.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Function to return the maximum value that can be put in a knapsack
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[n+1][W+1];
+
+    // Build table dp[][] in bottom-up manner
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i-1] <= w)
+                dp[i][w] = (val[i-1] + dp[i-1][w-wt[i-1]] > dp[i-1][w])
+                           ? val[i-1] + dp[i-1][w-wt[i-1]]
+                           : dp[i-1][w];
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+    int val[] = {60, 100, 120};   // values of items
+    int wt[] = {10, 20, 30};      // weights of items
+    int W = 50;                   // capacity of knapsack
+    int n = sizeof(val)/sizeof(val[0]);
+
+    printf("Maximum value in Knapsack = %d\n", knapsack(W, wt, val, n));
+    return 0;
+}
